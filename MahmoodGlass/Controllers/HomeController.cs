c04bc7c0e5cd5f89e6cfcec2c50ccac1a0c04fa1
tassawer-hussain <t.tassawer@gmail.com>
2023-11-04 02:55:50 +0500

    Initial Release to GitHub

---
+++ MahmoodGlass/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_Prof_Pro_Temp_Add_May16.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult portfolio()
+        {
+            return View();
+        }
+        public ActionResult services()
+        {
+            return View();
+        }
+        public ActionResult aboutus()
+        {
+            return View();
+        }
+
+        public ActionResult contactus()
+        {
+            return View();
+        }
+    }
+}
---
+++ MahmoodGlass/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MahmoodGlass.MvcApplication" Language="C#" %>
